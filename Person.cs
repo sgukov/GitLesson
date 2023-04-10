61831f2d114112cadc48bc51d20144b4723d9928
sgukov <sg_brui@mail.ru>
2023-04-10 19:34:01 +0300

    Edit Person.cs

--- Person.cs
+++ Person.cs
@@ -1,4 +1,11 @@
 class Person {
+    private string name = "none";
+    public int Age { get; set; }
     
+    Person() {}
+
+    public string GetName() { return name; }
+
+    public void SetName(string name) { this.name = name; }
 
 }
